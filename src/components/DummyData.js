cdd7032458d741f41294855a8a187b953e761647
Julfikar-Haidar <julfikar6262@gmail.com>
2022-08-03 14:55:54 +0600

    rectionadd

--- src/components/DummyData.js
+++ src/components/DummyData.js
@@ -38,17 +38,21 @@ export default function DummyData() {
     </tr> */}
     {
 data?.map((item,i) => (
-  <tr key={i}>
+  <tr key={i} className="rection_row">
     <td>{item.server}</td>
     <td>{item.channel}</td>
     <td>{item.sender}</td>
     <td>{item.message}</td>
     <td>{item.time}</td>
     <td>{item.date}</td>
-    <td>{item.reaction?.map((r, index) => (
-            <p key={index}>
-              {r.reaction}
+    <td className='rec_clm'>{item.counter?.map((r, index) => (
+            <p key={index} >
+              
+              <button >{r.key} {r.count} </button>
+        
             </p>
+           
+
           ))
     }
     </td>
@@ -58,6 +62,15 @@ data?.map((item,i) => (
 }
   </tbody>
   </table>
+ {/* <div className="grid-container">
+  <button>Button1</button>
+  <button>Button2</button>
+  <button>Button3</button>
+  <button>Button4</button>
+  <button>Button5</button>
+  <button>Button6</button>
+</div> */}
+
 
     </>
   )
